48fd3117f5d81fbc15883e0c018cde3eee233761
Anton.Satarov <Anton.Satarov@nedra.digital>
2022-04-18 18:30:36 +0300

    lesson 7

---
+++ src/test/java/allure/LambdaStepTest.java
@@ -0,0 +1,49 @@
+package allure;
+
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class LambdaStepTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE_NUMBER = 68;
+
+    @Test
+    public void testGithubIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем главную страницу", () -> {
+            open("https://github.com/");
+        });
+        step("Ищем репозиторий" + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step("Переходим по ссылке репозитория" + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Кликаем на таб Issues", () -> {
+            $(partialLinkText("Issues")).click();
+        });
+        step("Ищем Issue с номером" + ISSUE_NUMBER, () -> {
+            $(withText("#76")).click();
+            Allure.getLifecycle().addAttachment(
+                    "Исходники страницы",
+                    "text/html",
+                    "html",
+                    WebDriverRunner.getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8)
+            );        });
+    }
+}
