eca90b66a29ee2c86a6b8cd7012e79360a2b5315
Anton.Satarov <Anton.Satarov@nedra.digital>
2022-04-29 14:56:42 +0300

    added drag&drop test

---
+++ src/test/java/herokuapp/DragAndDropTest.java
@@ -0,0 +1,17 @@
+package herokuapp;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @Test
+    void swapTwoRectangles() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
