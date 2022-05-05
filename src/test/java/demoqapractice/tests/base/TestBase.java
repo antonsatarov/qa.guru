4a0032121a4e3d9b7c3b33772ef6f4b6d3ae1459
Anton.Satarov <Anton.Satarov@nedra.digital>
2022-05-05 18:11:29 +0300

    hided credentials

--- src/test/java/demoqapractice/tests/base/TestBase.java
+++ src/test/java/demoqapractice/tests/base/TestBase.java
@@ -2,20 +2,25 @@ package demoqapractice.tests.base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import demoqapractice.config.CredentialsConfig;
 import demoqapractice.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.apache.xmlbeans.SystemProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
+
     @BeforeAll
+    @Tag("owner")
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        CredentialsConfig config  = ConfigFactory.create(CredentialsConfig.class);
 
         Configuration.holdBrowserOpen = Boolean.parseBoolean(System.getProperty("holdBrowserOpen"));
         Configuration.baseUrl = System.getProperty("baseUrl");
@@ -23,7 +28,10 @@ public class TestBase {
         Configuration.browserSize = System.getProperty("browserSize");
         Configuration.browserVersion = System.getProperty("browserVersion");
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        String login = config.login();
+        String password = config.password();
+
+        Configuration.remote = "https://" + login + ":" + password + "@" + System.getProperty("selenoidHub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
