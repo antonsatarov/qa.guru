bb1effcbf4eec74f0cf55eadfad291fb8957ebdf
Anton.Satarov <Anton.Satarov@nedra.digital>
2022-05-05 11:08:33 +0300

    added video settings

--- src/test/java/demoqapractice/tests/base/TestBase.java
+++ src/test/java/demoqapractice/tests/base/TestBase.java
@@ -6,6 +6,7 @@ import demoqapractice.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -20,6 +21,11 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
 
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
